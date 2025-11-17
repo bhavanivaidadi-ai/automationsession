0c90fecb28ca0d9d65c208a0b91582bcaae2decc
RamchanderPoojari <ramchender.poojari@vassarlabs.com>
2025-11-17 12:49:24 +0530

    Commented some code

--- src/main/java/utils/Xpaths.java
+++ src/main/java/utils/Xpaths.java
@@ -9,12 +9,16 @@ public class Xpaths {
     public static By UuserName = By.xpath("//input[@placeholder='Username']");
     public static By Upassword = By.xpath("//input[@placeholder=\"Password\"]");
     public static By UloginButton = By.xpath("//button[text() = ' Login ']");
+    public static By errorMsgInvalid = By.xpath("//p[text()='Invalid credentials']");
+    public static By errMsgRequired = By.xpath("//span[text()='Required']");
+
 
     //profile
 
     public static By pimClcik = By.xpath("//a/span[text()='PIM']");
     public static By addEmpClick = By.xpath("//li/a[text()='Add Employee']");
     public static By addBtn = By.xpath("//button[text()=' Add ']");
+
     public static By empFName = By.xpath("//input[@name = 'firstName']");
     public static By empLName = By.xpath("//input[@placeholder='Last Name']");
     public static By empId = By.xpath("//label[text()='Employee Id']/ancestor::div[contains(@class,'oxd-input-group')]//input");
