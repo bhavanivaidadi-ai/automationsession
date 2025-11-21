81239b8d5ee6b0cafb7c49eb801e9756a1c46b8d
bhavanivaidadi-ai <bhavani.vaidadi@vassarlabs.com>
2025-11-21 15:25:49 +0530

    modified the code

--- src/main/java/utils/Xpaths.java
+++ src/main/java/utils/Xpaths.java
@@ -6,12 +6,15 @@ public class Xpaths {
 
 
     //login functionalitity with positive and negative cases
+
     public static By UuserName = By.xpath("//input[@placeholder='Username']");
     public static By Upassword = By.xpath("//input[@placeholder=\"Password\"]");
     public static By UloginButton = By.xpath("//button[text() = ' Login ']");
     public static By errorMsgInvalid = By.xpath("//p[text()='Invalid credentials']");
     public static By errMsgRequired = By.xpath("//span[text()='Required']");
     public static By validation = By.xpath("//span[text()='Dashboard']");
+
+
     //profile
 
     public static By pimClcik = By.xpath("//a/span[text()='PIM']");
